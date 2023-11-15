811a4513fdc0e79be45da0f170562f03d04a5814
NiiloP <paakkonenniilo@gmail.com>
2023-11-15 21:20:20 +0900

    SkipFrarme class and CustomEnv update

--- CustomEnv.py
+++ CustomEnv.py
@@ -17,8 +17,56 @@ class CustomEnv(PyBoyGymEnv):
       ]
 
     self._buttons_release = [
-      WindowEvent.PRESS_ARROW_RIGHT,
-      WindowEvent.PRESS_BUTTON_A
+      WindowEvent.RELEASE_ARROW_RIGHT,
+      WindowEvent.RELEASE_BUTTON_A
     ]
     self.actions = [self._DO_NOTHING] + self._buttons
     self.action_space = Discrete(len(self.actions))
+
+  # Step function for the agent including button release
+  # If the agent presses a button, it is toggled
+  # If the agent presses a button that is toggled, it is released  
+  def step(self, action_id):
+      info = {}
+
+      action = self.actions[action_id]
+      if action == self._DO_NOTHING:
+          pyboy_done = self.pyboy.tick()
+      else:
+          if self.action_type == "toggle":
+              if self._button_is_pressed[action]:
+                  self._button_is_pressed[action] = False
+                  action = self._release_button[action]
+              else:
+                  self._button_is_pressed[action] = True
+
+          self.pyboy.send_input(action)
+          pyboy_done = self.pyboy.tick()
+          
+          if self.action_type == "press":
+              print(self._release_button[action])
+              self.pyboy.send_input(self._release_button[action])
+
+      new_fitness = self.game_wrapper.fitness
+      reward = new_fitness - self.last_fitness
+      self.last_fitness = new_fitness
+
+      observation = self._get_observation()
+      done = pyboy_done or self.game_wrapper.game_over()
+
+      return observation, reward, done, info
+  
+  # Step function without button release
+  def stepSkip(self, action_id):
+      info = {}
+
+      pyboy_done = self.pyboy.tick()
+
+      new_fitness = self.game_wrapper.fitness
+      reward = new_fitness - self.last_fitness
+      self.last_fitness = new_fitness
+
+      observation = self._get_observation()
+      done = pyboy_done or self.game_wrapper.game_over()
+
+      return observation, reward, done, info
---
+++ SkipFrame.py
@@ -0,0 +1,29 @@
+import gym
+# We dont need to act on all of the frames so we can skip them beween actions
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        """Return only every `skip`-th frame"""
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+
+        """Repeat action, and sum reward"""
+        
+        # First do the initial action.
+        obs, reward, done, info = self.env.step(action)
+        total_reward = reward
+        
+        for i in range(self._skip-1):
+
+            # Accumulate reward and repeat the same action but without altering button presses
+            # If we used the normal step function, it would register button presses
+            # And the agent could not move at all
+            obs, reward, done, info = self.env.stepSkip(action)
+            
+            # Add the rewards together
+            total_reward += reward
+
+            if done:
+                break
+        return obs, total_reward, done,  info
