f93109fd5376ac92ec835df98ed9ec2910f9f6dc
NiiloP <paakkonenniilo@gmail.com>
2023-11-21 14:30:51 +0900

    Reworked reward completely, works Okay but could be better, added reset to CustomEnv, SkipFrame resets now previous state

--- SkipFrame.py
+++ SkipFrame.py
@@ -1,4 +1,5 @@
 import gym
+from GameState import GameState
 # We dont need to act on all of the frames so we can skip them beween actions
 class SkipFrame(gym.Wrapper):
     def __init__(self, env, skip):
@@ -13,13 +14,14 @@ class SkipFrame(gym.Wrapper):
         
         for i in range(self._skip):
 
-            # Accumulate reward and repeat the same action but without altering button presses
-            # If we used the normal step function, it would register button presses
-            # And the agent could not move at all
+            # Accumulate reward and repeat the same action
             obs, reward, done, info = self.env.step(action)
+
             # Add the rewards together
             total_reward += reward
-
             if done:
                 break
+
+        # Update previous game state
+        self.env.prevState = GameState(self.pyboy)
         return obs, total_reward, done,  info
